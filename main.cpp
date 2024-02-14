ee0e2abd49bbad5380600d31cab89eac30b740d6
Avi Mizrahi <avi1mizrahi@gmail.com>
2024-02-14 11:48:05 +0200

    Catch out of range exception

--- main.cpp
+++ main.cpp
@@ -4,27 +4,33 @@
 
 int main() {
     unsigned size, n_mines;
-    std::cout << "WELCOME TO  - M I N E S W E E P E R -" << std::endl;
-    std::cout << "Enter the grid width: ";
-    std::cin >> size;
-    std::cout << std::endl << "Enter the number of mines: ";
-    std::cin >> n_mines;
-    std::cout << std::endl;
+    using std::cin, std::cout, std::endl;
+
+    cout << "WELCOME TO  - M I N E S W E E P E R -" << endl;
+    cout << "Enter the grid width: ";
+    cin >> size;
+    cout << endl << "Enter the number of mines: ";
+    cin >> n_mines;
+    cout << endl;
 
     Minesweeper minesweeper(size, n_mines);
-    minesweeper.print(std::cout, Minesweeper::reveal);
+    minesweeper.print(cout, Minesweeper::reveal);
 
-    std::cout << "Now, give x,y sequentially" << std::endl;
+    cout << "Now, give x,y sequentially" << endl;
 
     while (minesweeper.state() == Minesweeper::GameState::ONGOING) {
-        std::cout << minesweeper << std::endl;
+        cout << minesweeper << endl;
         unsigned x, y;
-        std::cin >> x >> y;
-        minesweeper.click(x, y);
+        cin >> x >> y;
+        try {
+            minesweeper.click(x, y);
+        } catch (std::out_of_range&) {
+            cout << "out of range, try again." << endl;
+        }
     }
 
-    minesweeper.print(std::cout, Minesweeper::reveal);
-    std::cout << (minesweeper.state() == Minesweeper::GameState::WIN ? "WIN!!!" : "LOST :(") << std::endl;
+    minesweeper.print(cout, Minesweeper::reveal);
+    cout << (minesweeper.state() == Minesweeper::GameState::WIN ? "WIN!!!" : "LOST :(") << endl;
 
     return 0;
 }
